49edbd6c8364425a611e300b3be92628984dc2ab
LucaSous <lucassousadasilva333@gmail.com>
2022-01-05 12:33:21 -0300

    text formatting

--- lib/components/visualizar_modal.dart
+++ lib/components/visualizar_modal.dart
@@ -7,57 +7,89 @@ class VisualizarModal extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      // ignore: avoid_unnecessary_containers, sized_box_for_whitespace
-      body: Container(
-        padding: const EdgeInsets.all(30),
-        height: 300,
-        child: SingleChildScrollView(
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.stretch,
-            children: [
-              const Text(
-                'Dados do Usuário',
-                style: TextStyle(fontSize: 26, fontWeight: FontWeight.bold),
+    return Container(
+      padding: const EdgeInsets.all(30),
+      height: 500,
+      child: SingleChildScrollView(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            const Text(
+              'Dados do Usuário',
+              style: TextStyle(fontSize: 26, fontWeight: FontWeight.bold),
+            ),
+            const SizedBox(
+              height: 20,
+            ),
+            const Text(
+              'Código: ',
+              style: TextStyle(
+                fontSize: 18,
               ),
-              const SizedBox(
-                height: 20,
+            ),
+            Text(
+              '${usuario['id']}',
+              style: const TextStyle(
+                fontSize: 18,
+                color: Color(0xFF757575),
               ),
-              Text(
-                'Código: ' '${usuario['id']}',
-                style: const TextStyle(
-                  fontSize: 18,
-                ),
+            ),
+            const Text(
+              'Nome: ',
+              style: TextStyle(
+                fontSize: 18,
               ),
-              Text(
-                'Nome: ' + usuario['nome'],
-                style: const TextStyle(
-                  fontSize: 18,
-                ),
+            ),
+            Text(
+              '${usuario['nome']}',
+              style: const TextStyle(
+                fontSize: 18,
+                color: Color(0xFF757575),
               ),
-              Text(
-                'Endereço: ' + usuario['endereco'],
-                style: const TextStyle(
-                  fontSize: 18,
-                ),
+            ),
+            const Text(
+              'Endereço: ',
+              style: TextStyle(
+                fontSize: 18,
               ),
-              Text(
-                'Cidade: ' + usuario['cidade'],
-                style: const TextStyle(
-                  fontSize: 18,
-                ),
+            ),
+            Text(
+              '${usuario['endereco']}',
+              style: const TextStyle(
+                fontSize: 18,
+                color: Color(0xFF757575),
               ),
-              Text(
-                'E-mail: ' + usuario['email'],
-                style: const TextStyle(
-                  fontSize: 18,
-                ),
+            ),
+            const Text(
+              'Cidade: ',
+              style: TextStyle(
+                fontSize: 18,
               ),
-              const SizedBox(
-                height: 18,
+            ),
+            Text(
+              '${usuario['cidade']}',
+              style: const TextStyle(
+                fontSize: 18,
+                color: Color(0xFF757575),
               ),
-            ],
-          ),
+            ),
+            const Text(
+              'E-mail: ',
+              style: TextStyle(
+                fontSize: 18,
+              ),
+            ),
+            Text(
+              '${usuario['email']}',
+              style: const TextStyle(
+                fontSize: 18,
+                color: Color(0xFF757575),
+              ),
+            ),
+            const SizedBox(
+              height: 18,
+            ),
+          ],
         ),
       ),
     );
